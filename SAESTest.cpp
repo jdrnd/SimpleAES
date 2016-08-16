8eb206dd3d9a40caa16e1561855d3a3aa197aadb
Joel Ruhland <joel@ruhland.solutions>
2016-08-16 10:53:03 -0400

    split out tests into seperate methods

--- SAESTest.cpp
+++ SAESTest.cpp
@@ -7,6 +7,7 @@ class SAESTest{
     private:
         SAES crypter;
 
+        // Tests key derivation, as well as key padding
         void testKeyDerrivation(){
 
             std::string key = "longkeydfsgsdfgdysdfsgsdf";
@@ -16,6 +17,8 @@ class SAESTest{
             assert(keyarr[15] == (uint8_t)key[15]);
 
             key =  "shorterpass";
+            keyarr = crypter.deriveKey(key);
+
             assert(keyarr[0] == uint8_t(key[0]));
             assert(keyarr[15] == 0);
 
@@ -26,17 +29,7 @@ class SAESTest{
             std::cout << "\n\n";
         }
 
-    public:
-
-        void test(){
-
-
-
-
-            for (int i = 0; i<16; i++){
-                std::cout<< (int)keyarr[i] << " ";
-            }
-            std::cout<< "\n\n";
+        void testIVgeneration(){
 
             crypter.setiv();
             std::cout << "Outputting IV" << " ";
@@ -44,22 +37,50 @@ class SAESTest{
                 std::cout << crypter.iv[i] << " ";
             }
             std::cout<< "\n\n";
+        }
 
-
+        // Tests creation and padding of blocks with data
+        void testBlockify(){
             crypter.blockify("datsdfsdfsfsdfsda");
-            std::cout << "Outputing block with size " << crypter.size << "  : ";
+
+            assert(crypter.blocks[0] == (uint8_t)'d');
+            assert(crypter.blocks[16] == (uint8_t)'a');
+            assert(crypter.blocks[18] == 0);
+            assert(crypter.blocks[31] == 0);
+
+            std::cout << "Outputing raw block with size " << crypter.size << ": ";
             for (int i = 0; i<32; i++){
-                std::cout << crypter.blocks[i] << " ";
+                std::cout << (int)crypter.blocks[i] << " ";
             }
             std::cout<< "\n\n";
+        }
 
-            crypter.encryptText("longkeydfsgsdfgdysdfsgsdf",
-                                (std::basic_string<char, char_traits < char>, allocator < char >> ()));
+        void testEnDeCrypt(){
+
+            crypter.encryptText("key","long datajdfbshgkdsfjkdshfdjs,f");
             std::cout<< "Outputing encrypted text: ";
-            for (int i = 0; i<32; i++){
+            for (int i = 0; i<48; i++){
                 std::cout << crypter.blocks[i] << " ";
             }
             std::cout << "\n\n";
+
+
+        }
+
+    public:
+
+        void runTests(){
+
+            testKeyDerrivation();
+            testIVgeneration();
+            testBlockify();
+            testEnDeCrypt();
+
+
+            /*
+            crypter.encryptText("longkeydfsgsdfgdysdfsgsdf",
+                                (std::basic_string<char, char_traits < char>, allocator < char >> ()));
+
 
             crypter.decryptText("longkeydfsgsdfgdysdfsgsf",
                                 (std::basic_string<char, char_traits < char>, allocator < char >> ()));
@@ -69,9 +90,10 @@ class SAESTest{
             }
 
             std::cout<<  "\n\n" << time(NULL) % 255;
+             */
         }
 };
 int main(){
     SAESTest tester;
-    tester.test();
+    tester.runTests();
 }
